900b9bb56e0ba001a551790d38374f52c776d7a5
Vishal Kalathil <kalathilvishal1@gmail.com>
2024-02-16 22:12:44 +0530

    modified:   ../../Cargo.lock

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,4 +1,6 @@
-use std::process::Output;
+use rand::Rng;
+use rand::RngCore;
+use rand_chacha::ChaCha8Rng;
 
 struct Neuron {
     bias: f32,
@@ -6,6 +8,10 @@ struct Neuron {
 }
 
 impl Neuron {
+    fn new() -> Self {
+        todo!()
+    }
+
     fn propogate(&self, inputs: &[f32]) -> Result<f32, String> {
         assert_eq!(
             inputs.len(),
@@ -23,9 +29,9 @@ impl Neuron {
         Ok((self.bias + output).max(0.0))
     }
 
-    pub fn random(input_size: usize) -> Self {
-        let bias = todo!();
-        let weights = (0..input_size).map(|_| todo!()).collect();
+    pub fn random(input_size: usize, rng: &mut dyn rand::RngCore) -> Self {
+        let bias = rng.gen_range(-1.0..=1.0);
+        let weights = (0..input_size).map(|_| rng.gen_range(-1.0..=1.0)).collect();
         Self { bias, weights }
     }
 }
@@ -34,6 +40,10 @@ struct Layer {
     neurons: Vec<Neuron>,
 }
 impl Layer {
+    fn new(neurons: Vec<Neuron>) -> Self {
+        todo!()
+    }
+
     fn propogate(&self, inputs: Vec<f32>) -> Vec<f32> {
         self.neurons
             .iter()
@@ -41,11 +51,12 @@ impl Layer {
             .collect()
     }
 
-    pub fn random(input_size: usize, output_size: usize) -> Self {
+    pub fn random(rng: &mut dyn RngCore, input_size: usize, output_size: usize) -> Self {
         let neurons = (0..output_size)
-            .map(|_| Neuron::random(input_size))
+            .map(|_| Neuron::random(input_size, rng))
             .collect();
-        Self { neurons }
+
+        Self::new(neurons)
     }
 }
 
@@ -64,7 +75,7 @@ impl Network {
             .fold(inputs, |inputs, layer| layer.propogate(inputs))
     }
 
-    pub fn random(layers: &[LayerTopology]) -> Self {
+    pub fn random(rng: &mut dyn RngCore, layers: &[LayerTopology]) -> Self {
         assert!(
             layers.len() > 1,
             "Checking if number of layers {} is greater than 1",
@@ -72,9 +83,31 @@ impl Network {
         );
         let layers = layers
             .windows(2)
-            .map(|layers| Layer::random(layers[0].neurons, layers[1].neurons))
+            .map(|layers| Layer::random(rng, layers[0].neurons, layers[1].neurons))
             .collect();
 
         Self { layers }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    mod random {
+        use super::*;
+        use rand::{random, SeedableRng};
+        use rand_chacha::ChaCha8Rng;
+
+        #[test]
+        fn test() {
+            let mut rng = ChaCha8Rng::from_seed(Default::default());
+            let neuron = Neuron::random(4, &mut rng);
+
+            assert_eq!(neuron.bias, -0.6255188);
+            assert_eq!(
+                neuron.weights,
+                &[0.67383957, 0.8181262, 0.26284897, 0.5238807,]
+            );
+        }
     }
 }
